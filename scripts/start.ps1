11ffc9cff244a3ed34125d960eb38a81793e5b6c
luniksss <nikolaeval316@gmail.com>
2026-03-04 19:10:12 +0300

    lab2 docker + nginx

--- scripts/start.ps1
+++
@@ -1,4 +0,0 @@
-Set-Location $PSScriptRoot\..
-Write-Host "Запуск всех сервисов через docker-compose..."
-docker-compose up -d
-Write-Host "Система запущена. Nginx доступен по адресу http://localhost:8080"
--- scripts/stop.ps1
+++
@@ -1,4 +0,0 @@
-Set-Location $PSScriptRoot\..
-Write-Host "Остановка всех сервисов..."
-docker-compose down
-Write-Host "Система остановлена."
