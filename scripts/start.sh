a566d34215ea622385f44545e495af5b28b6a6ca
luniksss <nikolaeval316@gmail.com>
2026-03-25 05:37:39 +0300

    pa2 nginx + healthcheck

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")/.."
-docker compose up -d
+docker compose up -d --build
 echo "Система запущена."
 echo "Nginx доступен по адресу http://localhost:8080"
 echo "Экземпляры приложения: http://localhost:8081 и http://localhost:8082"
